a745fd766ea3c957876dd478f5de74ceaf855b80
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2021-11-03 20:45:47 +0100

    getting weird

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+function btnClicked() {
+  document.getElementById("catImg").classList.toggle("clicked");
+  document.getElementById("heartBtn").classList.toggle("clicked");
+}
+var i = 0;
+
+function togglePlay() {
+  i++;
+  if (i%2 == 1) {
+   document.getElementById('audio').play();
+  } else{
+    document.getElementById('audio').pause();
+  }
+}
+
+
 function loadEvent() {
   console.log("page loaded");
+  document.getElementById("heartBtn").addEventListener("click", btnClicked);
+  document.getElementById('heartBtn').addEventListener('click', togglePlay);
 }
 
 window.addEventListener("load", loadEvent);
